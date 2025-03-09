0ef77c6ec058ee49657e0385b00c768da9254a8a
drh-14 <darrenh1134@gmail.com>
2025-03-09 19:36:00 -0400

    implemented all heap functions

--- heap.c
+++ heap.c
@@ -14,10 +14,53 @@ void *heapPush(Heap *h, Graph *g){
     h -> size++;
     h -> arr = realloc(h -> arr, h -> size * sizeof(Pair));
     h -> arr[h -> size - 1] = p; 
+    fixHeap(h);
 }
 
 Graph *heapTop(Heap *h){
     return h -> arr[0].g;
+}
+
+void minHeapify(Heap *h, int i){
+    int smallest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    if(left < h -> size && h -> arr[left].key < h -> arr[smallest].key){
+        smallest = left;
+    }
+    if(right < h -> size && h -> arr[right].key < h -> arr[smallest].key){
+        smallest = right;
+    }
+    if(smallest != i){
+        Pair p = h -> arr[i];
+        h -> arr[i] = h -> arr[smallest];
+        h -> arr[smallest] = p;
+        minHeapify(h, smallest);
+    }
+}
+
+void maxHeapify(Heap *h, int i){
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    if(left < h -> size && h -> arr[left].key < h -> arr[largest].key){
+        largest = left;
+    }
+    if(right < h -> size && h -> arr[right].key < h -> arr[largest].key){
+       largest = right;
+    }
+    if(largest != i){
+        Pair p = h -> arr[i];
+        h -> arr[i] = h -> arr[largest];
+        h -> arr[largest] = p;
+        maxHeapify(h, largest);
+    }
+}
+
+void *fixHeap(Heap *h){
+    for(int i = h -> size / 2 - 1; i >= 0; i--){
+        h -> type ? minHeapify(h, i) : maxHeapify(h, i);
+    }
 }
 
 Graph *heapPop(Heap *h){
@@ -28,8 +71,7 @@ Graph *heapPop(Heap *h){
         h -> size--;
         h -> arr = realloc(h -> arr, sizeof(Pair) * h -> size);
     }
-    // heapify
-
+    fixHeap(h);
     return g;
 }
 
